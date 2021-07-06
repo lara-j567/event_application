41f53e3a772430a9ac96db35c9e84db28ca1265d
lara-j567 <l164052@lhr.nu.edu.pk>
2021-07-07 03:21:41 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import $ from "jquery";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    const [artistName, setArtistName] = useState("");
+    const [artistInformation, setArtistInformation] = useState({});
+    const [artistEvents, setArtistEvents] = useState([{}]);
+    
+    
+    const fetchData = () => {
+        
+        ///const URL =`https://app.swaggerhub.com/apis/Bandsintown/PublicAPI/3.0.0#/ArtistData/artists/${artistName}?app_id=aa817e4ec06a8d24666ea63289868366`;
+        const URL = `https://rest.bandsintown.com/artists/${artistName}?app_id=aa817e4ec06a8d24666ea63289868366`;
+        return new Promise((resolve, reject) => {
+            
+            $.ajax({
+                
+                url: URL,
+                async: false,
+                type: "get",
+                dataType: "json",
+                
+                success: function (result) {
+                    
+                    console.log(artistName);
+                    console.log(result);
+                    setArtistInformation(result);
+                    console.log('set artist information');
+                    console.log(artistInformation);
+                    
+                },
+                
+                error: function (error) {
+                    
+                    console.log(error);
+                    reject(error);
+                    
+                },
+                
+                
+            });
+            
+            
+        });
+        
+    }
+    
+    const handleSubmit = (e) => {
+        
+        e.preventDefault();
+        //  setArtistName(e.target.value);
+        console.log(artistName);
+        
+    }
+    
+    
+    return (
+        <div className="App">
+            <header className="title">
+                
+                <h1 className="heading">Event Getter</h1>
+                <p className="description">Tracking all the events happening near you!!</p>
+            
+            
+            </header>
+            
+            <section>
+                
+                <div className="form">
+                    
+                    <form onSubmit={handleSubmit}>
+                        
+                        <label>Artist name:</label>
+                        <input
+                            placeholder="search artist"
+                            type="text"
+                            value={artistName}
+                            onChange={(e) => setArtistName(e.target.value)}
+                            required
+                        
+                        />
+                        
+                        <button onClick={fetchData}>SUBMIT</button>
+                    
+                    
+                    </form>
+                
+                
+                </div>
+            
+            
+            </section>
+        </div>
+    );
 }
 
 export default App;
